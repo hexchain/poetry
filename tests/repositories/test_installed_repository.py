afb7259ecaa0c01ccd5e89f17e4f8d5292818249
Haochen Tong <i@hexchain.org>
2020-11-20 23:10:46 +0100

    Bump pytest-mock to 3.3.1

--- tests/repositories/test_installed_repository.py
+++ tests/repositories/test_installed_repository.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import pytest
 
-from pytest_mock.plugin import MockFixture
+from pytest_mock.plugin import MockerFixture
 
 from poetry.core.packages import Package
 from poetry.repositories.installed_repository import InstalledRepository
@@ -53,7 +53,7 @@ def env():  # type: () -> MockEnv
 
 
 @pytest.fixture
-def repository(mocker, env):  # type: (MockFixture, MockEnv) -> InstalledRepository
+def repository(mocker, env):  # type: (MockerFixture, MockEnv) -> InstalledRepository
     mocker.patch(
         "poetry.utils._compat.metadata.Distribution.discover",
         return_value=INSTALLED_RESULTS,
